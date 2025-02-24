e860791021a3c8eff3c0d102ec4b048195b8c716
Khang Kyle Huynh <khanghuynh@Khangs-MacBook-Pro.local>
2025-02-24 13:50:12 -0600

    pretty sure I messed up this repository. It should just be the FizzBuzz file.

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+string FizzBuzz(int number)
+{
+     var answer = "";
+     
+    if (number % 3 == 0)
+    {
+         answer = "fizz";
+    }
+
+    if (number % 5 == 0)
+    {
+        answer = "buzz";
+    }
+        
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+         answer = "fizzbuzz";
+    }
+    return answer;
+}
+
+//when the number is divisible by 3 return the word fizz
+
+//when the number is divisible by 5 return the word buzz
+
+//when the number is divisible by both, return the word fizzbuzz
